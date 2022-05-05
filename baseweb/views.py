a77eea9f52764dfa12d3b3e3dcec90d0ed33ca9d
aimwps <mwpskinner@gmail.com>
2022-05-05 17:13:24 +0100

    finished file upload form

--- baseweb/views.py
+++ baseweb/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView, View
 from .models import DocumentUpload
 from .forms import DocumentUploadForm
-
+from django.http import HttpResponseRedirect
 class LandingPageView(TemplateView):
     template_name = "landing_page.html"
 
@@ -15,3 +15,22 @@ class UploadPageView(View):
         context['unprocessed_uploads'] = DocumentUpload.objects.filter(has_been_converted=False)
         context['form'] = self.form_class
         return render(request, self.template_name, context)
+
+    def post(self, request):
+        if request.method == "POST":
+            print(request.POST)
+            form = DocumentUploadForm(request.POST, request.FILES)
+            if form.is_valid():
+                print("valid")
+                form.save()
+            else:
+
+                print(form)
+                print("Invalid Form")
+                context = {}
+                context['unprocessed_uploads'] = DocumentUpload.objects.filter(has_been_converted=False)
+                context['form'] = form
+                return render(request, self.template_name, context)
+
+
+            return redirect('/uploads/')
