bd7364004edf15e41dbaee27ec21bbc9a3c97ea9
aimwps <mwpskinner@gmail.com>
2022-05-24 14:54:38 +0100

    ..

--- existing_premiums_api/models.py
+++ existing_premiums_api/models.py
@@ -2,17 +2,6 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
-## Client Instructions for modelling data
-# Create a database to retain data for existing medical inusurance premiums
-    # Add risk factors(RF) that are used to calculate the premium.
-        # The age of the proposer as an integer
-        # The gender of the proposer, male or female
-        # The BMI of the proposer, a rational number to 1 decimal place
-        # The number of children the proposer has as an integer
-        # The smoker status of the proposer, true or false
-        # The region where the proposer, described as a point of a compass
-    # The premium for the policy - a rational number to 2 decial places
-
 class ExistingMedicalPremium(models.Model):
     gender_choices = (("female", "Female"),("male", "Male"),)
     region_choices = (("northwest", "North West"), ("northeast", "North East"), ("southeast", "South East"), ("southwest", "South West"))
